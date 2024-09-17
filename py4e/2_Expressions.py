fc19edf43c72a193d358e25135f6182949567c27
JRLemker <josephrlemker@gmail.com>
2024-09-17 12:03:02 -0500

    part 2

---
+++ py4e/2_Expressions.py
@@ -0,0 +1,13 @@
+# Exercise 1
+5
+x = 5
+print(x)
+x + 1  # note this has no effect
+print(x)
+x = x + 1
+print(x)
+
+# Exercise 2
+name = input('Enter you name:')
+print("Hello", name)  # note a space is automatically added
+
